315983e174e35c71e9a3a50cbb00fd415025acf1
selrvk <charles.a7cantara@gmail.com>
2026-06-11 09:24:23 -0400

    axios for general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,58 +1,54 @@
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-const { users } = require("./auth_users.js");
-const public_users = express.Router();
+const axios = require('axios');
 
-
-// Register a new user
-public_users.post("/register", (req, res) => {
-    const { username, password } = req.body;
-    
-    // Check if user already exists
-    const userExists = users.find(user => user.username === username);
-    
-    if (userExists) {
-        return res.status(400).json({ message: "User already exists!" });
+// Helper function to simulate/call your own API
+async function fetchFromAPI(endpoint) {
+    try {
+        const response = await axios.get(`http://localhost:5000/${endpoint}`);
+        return response.data;
+    } catch (error) {
+        throw error;
     }
-    
-    if (username && password) {
-        users.push({ "username": username, "password": password });
-        console.log("User registered! Total users:", users); // Debugging
-        return res.status(200).json({ message: "User successfully registered. You can now login." });
+}
+
+// Get the book list using async-await
+public_users.get('/', async function (req, res) {
+    try {
+        const booksData = await fetchFromAPI('');
+        return res.status(200).json(booksData);
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching books" });
     }
-    return res.status(400).json({ message: "Username and password are required." });
 });
 
-// Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    return res.status(200).send(JSON.stringify(books, null, 4));
+// Get book details based on ISBN using async-await
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+        const book = await fetchFromAPI(`isbn/${isbn}`);
+        return res.status(200).json(book);
+    } catch (error) {
+        return res.status(404).json({ message: "Book not found" });
+    }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+// Get book details based on author using async-await
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const booksByAuthor = await fetchFromAPI(`author/${author}`);
+        return res.status(200).json(booksByAuthor);
+    } catch (error) {
+        return res.status(404).json({ message: "Author not found" });
+    }
 });
 
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    const booksByAuthor = Object.values(books).filter(book => book.author === author);
-    return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
+// Get all books based on title using async-await
+public_users.get('/title/:title', async function (req, res) {
+    try {
+        const title = req.params.title;
+        const booksByTitle = await fetchFromAPI(`title/${title}`);
+        return res.status(200).json(booksByTitle);
+    } catch (error) {
+        return res.status(404).json({ message: "Title not found" });
+    }
 });
-
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    const booksByTitle = Object.values(books).filter(book => book.title === title);
-    return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
-});
-
-// Get book review
-public_users.get('/review/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
-});
-
-module.exports.general = public_users;
